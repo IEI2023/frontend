a2cdbedb5401844ec102bb5b855a652a86add78b
EMAKROU <emakrou@etsinf.upv.es>
2024-01-10 15:43:27 +0100

    Front sin conectar a la api

---
+++ Busqueda/busqueda.js
@@ -0,0 +1,104 @@
+var map = L.map("map").setView([40.416775, -3.70379], 6);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+// Límites para el mapa de España
+var southWest = L.latLng(36.0, -9.5), // Coordenadas aproximadas del extremo suroeste de España
+  northEast = L.latLng(43.5, 3.5), // Coordenadas aproximadas del extremo noreste de España
+  bounds = L.latLngBounds(southWest, northEast);
+
+map.setMaxBounds(bounds); // Restringe la vista a estos límites
+
+// Centros educativos - Ejemplo de array
+var centrosEducativos = [
+  // Añade tus centros educativos aquí
+  {
+    latitud: 40.416775,
+    longitud: -3.70379,
+    nombre: "Centro Educativo Madrid",
+    tipo: "Público",
+  },
+  {
+    latitud: 41.387918266,
+    longitud: 2.139598772,
+    nombre: "Centro Educativo Barcelona",
+    tipo: "Público",
+  },
+  // ...otros centros...
+];
+
+// Añadir marcadores para cada centro educativo
+centrosEducativos.forEach(function (centro) {
+  var popupContent = centro.nombre + "<br>Tipo: " + centro.tipo;
+  L.marker([centro.latitud, centro.longitud])
+    .bindPopup(popupContent)
+    .addTo(map);
+});
+
+function cancelar() {
+  document.getElementById("selector").value = "0";
+  document.getElementById("cod_postal").value = "";
+  document.getElementById("localidad").value = "";
+  document.getElementById("provincia").value = "";
+  document.getElementById("aceptar").disabled = true;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let loc = document.getElementById("localidad");
+  let cod = document.getElementById("cod_postal");
+  let prov = document.getElementById("provincia");
+  loc.addEventListener("input", updateValueLoc);
+  cod.addEventListener("input", updateValueCod);
+  prov.addEventListener("input", updateValueProv);
+});
+
+function updateValueLoc(e) {
+  cod_length = document.getElementById("cod_postal").value.length;
+  prov_lenth = document.getElementById("provincia").value.length;
+
+  if (cod_length != 0 && cod_length != 5) {
+    document.getElementById("aceptar").disabled = true;
+    return;
+  }
+
+  if (e.target.value.length != 0 || prov_lenth != 0 || cod_length == 5) {
+    document.getElementById("aceptar").disabled = false;
+  } else {
+    document.getElementById("aceptar").disabled = true;
+  }
+}
+
+function updateValueCod(e) {
+  loc_length = document.getElementById("localidad").value.length;
+  prov_lenth = document.getElementById("provincia").value.length;
+
+  if (e.target.value.length != 0 && e.target.value.length != 5) {
+    document.getElementById("aceptar").disabled = true;
+    return;
+  }
+
+  if (loc_length != 0 || prov_lenth != 0 || e.target.value.length == 5) {
+    document.getElementById("aceptar").disabled = false;
+  } else {
+    document.getElementById("aceptar").disabled = true;
+  }
+}
+
+function updateValueProv(e) {
+  loc_length = document.getElementById("localidad").value.length;
+  cod_length = document.getElementById("cod_postal").value.length;
+
+  if (cod_length != 0 && cod_length != 5) {
+    document.getElementById("aceptar").disabled = true;
+    return;
+  }
+
+  if (loc_length != 0 || e.target.value.length != 0 || cod_length == 5) {
+    document.getElementById("aceptar").disabled = false;
+  } else {
+    document.getElementById("aceptar").disabled = true;
+  }
+}
--- Carga/carga.js
+++ Carga/carga.js
@@ -1,1 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const selectAllCheckbox = document.getElementById("select-all");
+  const itemCheckboxes = document.querySelectorAll(".checkbox-item");
 
+  selectAllCheckbox.addEventListener("change", function () {
+    itemCheckboxes.forEach((checkbox) => {
+      checkbox.checked = this.checked;
+    });
+
+    if (this.checked) {
+      document.getElementById("aceptar").disabled = false;
+    } else {
+      document.getElementById("aceptar").disabled = true;
+    }
+  });
+
+  itemCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      if (!this.checked) {
+        selectAllCheckbox.checked = false;
+      } else {
+        const allChecked = Array.from(itemCheckboxes).every((cb) => cb.checked);
+        selectAllCheckbox.checked = allChecked;
+      }
+
+      let murcia = document.getElementById("murcia");
+      let valencia = document.getElementById("valencia");
+      let catalunya = document.getElementById("catalunya");
+
+      if (murcia.checked || valencia.checked || catalunya.checked) {
+        document.getElementById("aceptar").disabled = false;
+      } else {
+        document.getElementById("aceptar").disabled = true;
+      }
+    });
+  });
+});
+
+function reset() {
+  const selectAllCheckbox = document.getElementById("select-all");
+  let murcia = document.getElementById("murcia");
+  let valencia = document.getElementById("valencia");
+  let catalunya = document.getElementById("catalunya");
+
+  selectAllCheckbox.checked = false;
+  murcia.checked = false;
+  valencia.checked = false;
+  catalunya.checked = false;
+  document.getElementById("aceptar").disabled = true;
+}
